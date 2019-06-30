3e0c87cdac124fbb9d0585d013e0f76f8c4d7496
yczh <yc_zhu@foxmail.com>
2019-06-30 15:25:27 +0800

    genpdf.js

--- backend/genpdf.js
+++ backend/genpdf.js
@@ -8,6 +8,7 @@ var zip = new JSZip(content);
 var doc = new Docxtemplater();
 var util = require('./util');
 
+var toPdf = require("office-to-pdf");
 
 module.exports.getPdfApplication=function (db, info,res) {
     console.log('generate pdf file');
@@ -91,6 +92,14 @@ module.exports.getPdfApplication=function (db, info,res) {
             var buf = doc.getZip().generate({ type: 'nodebuffer' });
             /* buf is a nodejs buffer, you can either write it to a file or do anything else with it.*/
             fs.writeFileSync(path.join(__dirname, '../data/out/申请'+application.id.toString()+ '.docx'), buf);
+
+            toPdf(buf).then(
+                (pdfBuffer) => {
+                    fs.writeFileSync("./test.pdf", pdfBuffer)
+                }, (err) => {
+                    console.log(err)
+                }
+            )
 
         }
     });
