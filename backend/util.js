c982b1fa481b8ed8892957369c6eddd7d69ac621
goodapple8946 <goodapple8946@163.com>
2019-07-01 12:49:02 +0800

    time form

--- backend/util.js
+++ backend/util.js
@@ -7,5 +7,16 @@ module.exports.values = function(num) {
 
 module.exports.getTime = function() {
     var time = new Date();
-    return time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate() + ' ' + time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+    var year = time.getFullYear();
+    var month = time.getMonth() + 1;
+    if (month < 10) month = '0' + month;
+    var date = time.getDate();
+    if (date < 10) date = '0' + date;
+    var hour = time.getMinutes();
+    if (hour < 10) hour = '0' + hour;
+    var minute = time.getMinutes();
+    if(minute < 10) minute = '0' + minute;
+    var second = time.getSeconds();
+    if(second < 10) second = '0' + second;
+    return year + '-' + month + '-' + date + ' ' + hour + ':' + minute + ':' + second;
 }
