eede39e6532f47121fb78e6bf1058307111c83d3
goodapple8946 <goodapple8946@163.com>
2019-07-01 12:50:36 +0800

    nope its wrong

--- backend/util.js
+++ backend/util.js
@@ -7,16 +7,16 @@ module.exports.values = function(num) {
 
 module.exports.getTime = function() {
     var time = new Date();
-    var year = time.getFullYear();
-    var month = time.getMonth() + 1;
+    var year = time.getFullYear().toString();
+    var month = (time.getMonth() + 1).toString();
     if (month < 10) month = '0' + month;
-    var date = time.getDate();
+    var date = time.getDate().toString();
     if (date < 10) date = '0' + date;
-    var hour = time.getMinutes();
+    var hour = time.getMinutes().toString();
     if (hour < 10) hour = '0' + hour;
-    var minute = time.getMinutes();
+    var minute = time.getMinutes().toString();
     if(minute < 10) minute = '0' + minute;
-    var second = time.getSeconds();
+    var second = time.getSeconds().toString();
     if(second < 10) second = '0' + second;
     return year + '-' + month + '-' + date + ' ' + hour + ':' + minute + ':' + second;
 }
